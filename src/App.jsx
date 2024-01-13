5b67b90472f677f53530b0dbfba53c6a7c83ea80
Mahima Pandey <mahimapandey44@gmail.com>
2024-01-13 18:20:30 +0530

    added new features

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import Search from "./components/Search"
 function App() {
 
   return (
-    <div className="grid place-items-center w-full h-full bg-black/85 pb-[10vh]">
-      <div className="mt-[10vh] w-[80vw] bg-blue-200 py-4 px-8 pb-[10vh] flex flex-col items-center rounded-md">
+    <div className="grid place-items-center w-full h-full pb-[10vh]">
+      <div className="mt-[10vh] w-[90vw] bg-blue-200 py-4 px-8 pb-[10vh] flex flex-col items-center rounded-md">
         <Search/>
       </div> 
     </div>
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,44 +1,74 @@
 import { useRef, useState } from 'react';
 import { RxCross1 } from "react-icons/rx";
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 import axios from "axios";
-import {data} from "./data"
+
 const BASE_URL="https://api.unsplash.com/search/photos";
-const IMAGES_PER_PAGE=20;
+const IMAGES_PER_PAGE=80;
+
 const Search = () => {
     const inputRef=useRef("");
-    const [images,setImages]=useState(data.results);
-    
+    const [page,setPage]=useState(1);
+    const [totalPages,setTotalPages]=useState(1);
+    const [images,setImages]=useState([]);
+    const [queryData,setQueryData]=useState("Search for any image !");
+
     const searchPics= async (inp)=>{
         try{
-            const url=`${BASE_URL}?query=${inp}&page=1&per_page=${IMAGES_PER_PAGE}&client_id=${import.meta.env.VITE_ACCESS_KEY}`
+            setQueryData("Loading...");
+            const url=`${BASE_URL}?query=${inp}&page=${page}&per_page=${IMAGES_PER_PAGE}&client_id=${import.meta.env.VITE_ACCESS_KEY}`
             const {data}=await axios.get(url);
-            console.log(data);
             setImages(data.results);
+            setTotalPages(data.total_pages);
+            if(data.total_pages===0){
+                setQueryData("No images found! Search for something else..")
+            }
         }
         catch(error){
             console.log(error);
+            setImages([]);
+            setQueryData(error.message);
         }
     }
     return (
     <div>
         <h1 className='text-center font-semibold text-4xl mb-2'>ImagifyHub</h1>
-        <div className='flex justify-center mb-8'>
-        <div className='border-2 border-black rounded-l-md bg-white'>
-        <input ref={inputRef} type='text' placeholder='search for images..' className='w-[40vw] rounded-l-md focus:outline-none py-1 px-4'/>
-        <button className=" py-2 px-2 text-black" onClick={()=>{
-            inputRef.current.value="";
-        }}><RxCross1/></button>
+        <div className='flex w-full justify-center'>
+        <div className='flex flex-col sm:flex-row gap-4 sm:gap-0 justify-center mb-16 w-[70vw] md:w-[60vw] lg:w-[50vw]'>
+            <div className='border-2 border-black rounded-md sm:rounded-r-none bg-white flex justify-between basis-3/4' >
+                <input ref={inputRef} type='text' placeholder='search for images..' className='rounded-md sm:rounded-r-none focus:outline-none py-1 px-4 w-full '/>
+                <button className=" py-2 px-2 text-black" onClick={()=>{
+                    inputRef.current.value="";
+                }}><RxCross1 size={20}/></button>
+            </div>
+            <button className="flex bg-blue-700 rounded-md sm:rounded-l-none py-2 px-8 text-white font-medium  justify-center basis-1/4" onClick={()=>{
+                setPage(1);
+                setImages([]);
+                searchPics(inputRef.current.value);
+            }}>Search</button>
         </div>
-        <button className="flex bg-blue-800 rounded-r-md py-2 px-8 text-white font-medium items-center" onClick={()=>{
-            searchPics(inputRef.current.value)
-        }}>Search</button>
         </div>
-        {inputRef.current.value?<h1 className='text-center font-semibold text-3xl mb-4'>Images for {inputRef.current.value}</h1>:<h1 className='text-center font-semibold text-3xl mb-4'>Images for cats</h1>}
-        <div className='grid grid-cols-4 gap-x-8 gap-y-12 justify-items-center'>
-            {images.map((elem)=>{
-                return <img className='object-cover rounded-md h-[15rem] w-[15rem]' key={elem.id} src={elem.urls.small} alt={elem.alt_desciption} />
-            })}
-        </div>
+        
+        {images.length===0 ? <div className='text-center text-2xl font-semibold '>{queryData}</div> : <div>
+            <h1 className='text-center font-semibold text-3xl mb-4'>Images for {inputRef.current.value}</h1>
+            <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-x-8 gap-y-12 justify-items-center'>
+                {images.map((elem)=>{
+                    return <LazyLoadImage className='object-cover rounded-md h-[15rem] w-[15rem]' effect='blur' key={elem.id}  src={elem.urls.small} alt={elem.alt_desciption} />
+                })}
+            </div>
+            <div className='flex justify-center mt-6'>
+                <button className='px-6 py-1 rounded-md bg-blue-700 text-white font-semibold text-md sm:text-xl lg:py-2' onClick={()=>{
+                    page===1 ? setPage(totalPages) : setPage(page-1);
+                    searchPics(inputRef.current.value);
+                }}>Prev</button>
+                <h2 className='font-medium px-6 text-md sm:text-xl lg:2xl lg:py-2'>{`page ${page} of ${totalPages}`}</h2>
+                <button className='px-6 rounded-md lg:py-2 bg-blue-700 text-white font-semibold text-md sm:text-xl' onClick={()=>{
+                    page===totalPages ? setPage(1): setPage(page+1);
+                    searchPics(inputRef.current.value);
+                }}>Next</button>
+            </div>
+        </div>}
     </div>
     )
 }
